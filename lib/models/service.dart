fca56336d8f02967e46a99d873739c932498e98b
maria9501 <41512076+maria9501@users.noreply.github.com>
2025-05-26 17:36:43 +0300

    Add lib/models/service.dart 1748270203908

---
+++ lib/models/service.dart
@@ -0,0 +1,17 @@
+class Service {
+  final int id;
+  final String title;
+  final String description;
+  final String avgPrice;
+  final String imageUrl;
+  final String icon;
+
+  Service({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.avgPrice,
+    required this.imageUrl,
+    required this.icon,
+  });
+}
